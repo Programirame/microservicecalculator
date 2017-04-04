79b40008436a54a9dcb012bc299cee54869000b9
Igor Stojanovski <igorce@outlook.com>
2017-04-04 21:21:40 +0200

    Dockerize deployments
    Dockerize the deployment of each of the separate services.
    
    Add a docker-compose for running the whole project.

---
+++ addition/src/main/java/org/programirame/AdditionController.java
@@ -0,0 +1,19 @@
+package org.programirame;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/add")
+@RestController
+public class AdditionController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Integer> addTwoNumbers(@RequestParam(value = "x") int x, @RequestParam(value = "y") int y) {
+        return new ResponseEntity<>(x+y, HttpStatus.OK);
+    }
+
+}
